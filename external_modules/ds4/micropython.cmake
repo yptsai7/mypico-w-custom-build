a9773638860f0610dfed521a6c957654780234f7
yptsai7 <yptsai7@gmail.com>
2026-03-15 18:59:32 +0800

    Update usermod target in micropython.cmake

--- external_modules/ds4/micropython.cmake
+++ external_modules/ds4/micropython.cmake
@@ -1,2 +1,3 @@
-target_sources(usermod_rp2 INTERFACE ${CMAKE_CURRENT_LIST_DIR}/ds4.c)
-target_include_directories(usermod_rp2 INTERFACE ${CMAKE_CURRENT_LIST_DIR})
+# 使用 usermod 目標，這會由 MicroPython 的主要建構腳本自動處理
+target_sources(usermod INTERFACE ${CMAKE_CURRENT_LIST_DIR}/ds4.c)
+target_include_directories(usermod INTERFACE ${CMAKE_CURRENT_LIST_DIR})
