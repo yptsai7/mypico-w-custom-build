c8293342be0c5913b59dc04a02a0f8d27482fdaa
yptsai7 <yptsai7@gmail.com>
2026-03-16 08:17:13 +0800

    Update micropython.cmake

--- external_modules/ds4/micropython.cmake
+++ external_modules/ds4/micropython.cmake
@@ -1,3 +1,16 @@
-# 使用 usermod 目標，這會由 MicroPython 的主要建構腳本自動處理
-target_sources(usermod INTERFACE ${CMAKE_CURRENT_LIST_DIR}/ds4.c)
-target_include_directories(usermod INTERFACE ${CMAKE_CURRENT_LIST_DIR})
+target_sources(usermod INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/ds4.c
+)
+
+target_include_directories(usermod INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}
+)
+
+# 連結 BTstack 和 Pico W 必要函式庫
+target_link_libraries(usermod INTERFACE
+    pico_btstack_classic
+    pico_btstack_hid
+    pico_btstack_ble
+    pico_cyw43_arch_threadsafe_background
+    pico_multicore
+)
