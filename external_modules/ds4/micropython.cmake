0ce4a472e75e2eca3ab60fd539dd6f58120979fe
yptsai7 <yptsai7@gmail.com>
2026-03-16 16:14:26 +0800

    Update micropython.cmake

--- external_modules/ds4/micropython.cmake
+++ external_modules/ds4/micropython.cmake
@@ -1,10 +1,10 @@
-# 定義模組：ds4
 micropython_add_user_cmodule(ds4 INTERFACE SOURCES ${CMAKE_CURRENT_LIST_DIR}/ds4.c)
 
-# 連結必要的藍牙與核心庫
+# 關鍵：必須正確連結 btstack 相關的硬體抽象層
 target_link_libraries(usermod INTERFACE 
     pico_btstack_classic 
     pico_btstack_ble 
+    pico_btstack_cyw43
     pico_multicore
 )
 
