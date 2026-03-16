f8884ad6e43b96beab104b15fa6382a99033b4d7
yptsai7 <yptsai7@gmail.com>
2026-03-16 16:37:48 +0800

    Update micropython.cmake

--- external_modules/ds4/micropython.cmake
+++ external_modules/ds4/micropython.cmake
@@ -1,7 +1,7 @@
-# 註冊用戶模組 ds4
+# 將 ds4.c 註冊為名為 ds4 的用戶模組
 micropython_add_user_cmodule(ds4 INTERFACE SOURCES ${CMAKE_CURRENT_LIST_DIR}/ds4.c)
 
-# 連結必要的藍牙與硬體抽象層庫
+# 連結必要的庫，確保 Pico W 藍牙晶片 (cyw43) 能運作
 target_link_libraries(usermod INTERFACE 
     pico_btstack_classic 
     pico_btstack_ble 
