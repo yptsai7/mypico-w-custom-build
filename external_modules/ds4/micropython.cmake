8ff070a803e4da3e400c859fa532e852b7b5c872
yptsai7 <yptsai7@gmail.com>
2026-03-16 08:51:09 +0800

    Update micropython.cmake

--- external_modules/ds4/micropython.cmake
+++ external_modules/ds4/micropython.cmake
@@ -6,6 +6,11 @@ target_include_directories(usermod INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
+# 覆蓋 btstack_config.h 的搜尋路徑（優先使用我們的設定）
+target_compile_options(usermod INTERFACE
+    -include ${CMAKE_CURRENT_LIST_DIR}/btstack_config.h
+)
+
 # 連結 BTstack 和 Pico W 必要函式庫
 target_link_libraries(usermod INTERFACE
     pico_btstack_classic
@@ -14,3 +19,4 @@ target_link_libraries(usermod INTERFACE
     pico_cyw43_arch_threadsafe_background
     pico_multicore
 )
+
