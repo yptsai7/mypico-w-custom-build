e23e75b5e44c9a169f633ddabe0964c7a1940239
yptsai7 <yptsai7@gmail.com>
2026-03-16 09:25:31 +0800

    Update micropython.cmake

--- external_modules/ds4/micropython.cmake
+++ external_modules/ds4/micropython.cmake
@@ -6,10 +6,10 @@ target_include_directories(usermod INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
+# 只連結 BTstack Classic BT 相關
+# 不連結 pico_cyw43_arch_threadsafe_background（MicroPython 主程式已處理）
 target_link_libraries(usermod INTERFACE
     pico_btstack_classic
     pico_btstack_hid
-    pico_btstack_ble
-    pico_cyw43_arch_threadsafe_background
     pico_multicore
 )
