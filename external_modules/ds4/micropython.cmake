39e5e346021bdf2126e68826a09ae2cfc7ca5bc8
yptsai7 <yptsai7@gmail.com>
2026-03-16 15:47:19 +0800

    Update micropython.cmake

--- external_modules/ds4/micropython.cmake
+++ external_modules/ds4/micropython.cmake
@@ -1,7 +1,7 @@
-# 使用最新的 C 模組添加方法
+# 定義模組名稱為 ds4
 micropython_add_user_cmodule(ds4 INTERFACE SOURCES ${CMAKE_CURRENT_LIST_DIR}/ds4.c)
 
-# 連結 Pico SDK 的藍牙經典與多核支援
+# 連結必要的藍牙與多核庫
 target_link_libraries(usermod INTERFACE 
     pico_btstack_classic 
     pico_btstack_ble 
