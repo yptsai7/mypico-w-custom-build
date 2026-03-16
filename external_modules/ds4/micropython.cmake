5aa417c6328fa59b7ddd336e3eaf482c84b580ef
yptsai7 <yptsai7@gmail.com>
2026-03-16 15:58:06 +0800

    Update micropython.cmake

--- external_modules/ds4/micropython.cmake
+++ external_modules/ds4/micropython.cmake
@@ -1,12 +1,12 @@
-# 將當前目錄定義為名為 ds4 的用戶模組
+# 將 ds4 資料夾宣告為一個 MicroPython 用戶模組
 micropython_add_user_cmodule(ds4 INTERFACE SOURCES ${CMAKE_CURRENT_LIST_DIR}/ds4.c)
 
-# 連結 Pico SDK 的藍牙與多核庫到 usermod 全域目標
+# 連結 Pico SDK 的藍牙與多核支援至全域模組目標
 target_link_libraries(usermod INTERFACE 
     pico_btstack_classic 
     pico_btstack_ble 
     pico_multicore
 )
 
-# 確保包含路徑正確
+# 確保編譯器能找到當前目錄下的標頭檔
 target_include_directories(usermod INTERFACE ${CMAKE_CURRENT_LIST_DIR})
